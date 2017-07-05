14144ef67593590d0d9785460741412a1fd0f471
msrba <maik.srba@gwdg.de>
2017-07-05 13:22:23 +0200

    Add new file

---
+++ resource_gen.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def endpoint(context):
+    endpoint = {'url': 'testurl'}
+    return endpoint
+
+def deploy(context):
+    pass
+    
+def clean(context):
+    pass
